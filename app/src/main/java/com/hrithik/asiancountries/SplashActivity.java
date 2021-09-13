1b037ada9cfb4e361ec7489703f836df090df121
Coder481 <hrithiksharma24092000@gmail.com>
2021-09-13 14:51:33 +0530

    ui changes

---
+++ app/src/main/java/com/hrithik/asiancountries/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.hrithik.asiancountries;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
